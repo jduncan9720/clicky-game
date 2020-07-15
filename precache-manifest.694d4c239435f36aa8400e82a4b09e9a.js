665ad535649d536abe808fe73c703fba77f20e42
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-07-15 11:51:04 -0600

    Updates

---
+++ precache-manifest.694d4c239435f36aa8400e82a4b09e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a679f2ecf58ba41078fc2ce640880b3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bfc1d4db61b252432e1b",
+    "url": "/clicky-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2b1acc18504d53a7cf66",
+    "url": "/clicky-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bfc1d4db61b252432e1b",
+    "url": "/clicky-game/static/js/2.bf8bd490.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.bf8bd490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1acc18504d53a7cf66",
+    "url": "/clicky-game/static/js/main.40a380a2.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7407c5c55cc8363001b6b44139dcfeba.js"
+  "/clicky-game/precache-manifest.694d4c239435f36aa8400e82a4b09e9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
