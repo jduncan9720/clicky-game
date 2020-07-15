5fddba601dcc8e076abd0ad2aa92ac1f507db612
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-07-15 14:39:05 -0600

    Styling

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,6 +7,9 @@ class Navbar extends React.Component {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
               <div className="container">
                 <a className="navbar-brand text-uppercase" href="/">Clicky Game</a>
+                <div className="title">
+                  The Witcher Clicker
+                </div>
                 <h4 className="navbar-text">
                   Score: {this.props.score} | Top Score: {this.props.topScore}
                 </h4>
